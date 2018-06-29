10bb6f423ae18f85968a87b7906e7ac354d3221e
Kush Rastogi <krustey@gmail.com>
2018-06-28 21:52:14 -0700

    Added Login Credentials and Routing

--- app.js
+++ app.js
@@ -4,17 +4,31 @@ var express = require("express"); //Routing
 var logger = require("morgan"); //HTTP Logger
 //Data
 var requests = require("request"); //Used to request data from Spotify's Web API
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser'); //loads cookie parser library
+//Spotify Configurations [secret]
+var config = require('./config');
+var credentials = require("./controllers/credentials")
 
 var app = express();
 app.set("views", path.join(__dirname, "views")) //Lets system know that views are in the /views folder
 app.set("view engine", "hbs"); //Declares handlebars as the templating engine
 app.use(logger('dev')); //Using the morgan logger for HTTP requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 var router = express.Router(); //The router matches HTTP requests to frontend views
 
-router.get("/playlist", function(req, res, next) {
-	res.render("playlist");
-});
+app.use('/', router)
+
+// router.get('/', function(req, res, next) {
+// 	if (req.session.access_token) {
+// 		res.redirect('/main');
+// 	} else {
+// 		res.redirect('/login'); //incorporate session data soon
+// 	}
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
