0b4676214003b99e702ebd404464b9ddf1448ead
Kush Rastogi <krustey@gmail.com>
2018-07-01 17:58:28 -0700

    modified scopes to include playlist modify

--- controllers/routes.js
+++ controllers/routes.js
@@ -36,7 +36,7 @@ router.get('/login', function(req, res, next) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email playlist-read-private playlist-read-collaborative user-modify-playback-state';
+  var scope = 'user-read-private user-read-email playlist-read-private playlist-read-collaborative user-modify-playback-state playlist-modify-public playlist-modify-private';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
