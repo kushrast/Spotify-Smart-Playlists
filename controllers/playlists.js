652efb0ad28f1b93ce2f850cd95158007de7695f
Kush Rastogi <krustey@gmail.com>
2018-07-01 18:54:53 -0700

    using sortable event obj to find list

--- controllers/playlists.js
+++ controllers/playlists.js
@@ -49,32 +49,32 @@ router.get('/', function(req, res, next) {
   }
 });
 
-// router.post("/:uri/update", function(req, res, next) {
-//   console.log(req.session);
+router.post("/:uri/update", function(req, res, next) {
+  console.log(req.session);
 
-//   if (!req.session.access_token) {
-//     res.redirect("/invalid");
-//   } 
+  if (!req.session.access_token) {
+    res.redirect("/invalid");
+  } 
 
-//   else {
-//     var body = JSON.parse(Object.keys(req.body)[0]);
-//     var uri = req.params.uri.split(":"); //Done like this to parse out the user and playlist out of the uri
-//     var options = {
-//       url: 'https://api.spotify.com/v1/users/'+uri[2]+'/playlists/'+uri[4]+'/tracks',
-//       headers: { 'Authorization': 'Bearer ' + req.session.access_token },
-//       body: {
-//         'uris': body.uris
-//       },
-//       json: true
-//     };
+  else {
+    var body = JSON.parse(Object.keys(req.body)[0]);
+    var uri = req.params.uri.split(":"); //Done like this to parse out the user and playlist out of the uri
+    var options = {
+      url: 'https://api.spotify.com/v1/users/'+uri[2]+'/playlists/'+uri[4]+'/tracks',
+      headers: { 'Authorization': 'Bearer ' + req.session.access_token },
+      body: {
+        'uris': body.uris
+      },
+      json: true
+    };
 
-//     console.log(options);
+    console.log(options);
 
-//     // use the access token to access the Spotify Web API
-//     request.put(options, function(error, response, playlist) {
-//       console.log(error);
-//     });
-//   }
-// });
+    // use the access token to access the Spotify Web API
+    request.put(options, function(error, response, playlist) {
+      console.log(error);
+    });
+  }
+});
 
 module.exports = router;
