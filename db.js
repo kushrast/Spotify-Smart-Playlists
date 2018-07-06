607a7f63062c2838fcc63233b2c995bd00bd4815
Kush Rastogi <krustey@gmail.com>
2018-07-05 22:38:40 -0700

    small change to possibly out of date db code

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ exports.connect = function(url, done) {
 
   MongoClient.connect(url, function(err, db) {
     if (err) return done(err)
-    state.db = db
+    state.db = db.db;
     done()
   })
 }
