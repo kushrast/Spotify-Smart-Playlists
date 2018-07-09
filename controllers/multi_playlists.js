fea1ac1004ffd78e8724910c6dd8023041b1a3b0
Kush Rastogi <krustey@gmail.com>
2018-07-08 18:33:43 -0700

    added frontend code to add playlist to multi

--- controllers/multi_playlists.js
+++ controllers/multi_playlists.js
@@ -41,4 +41,10 @@ router.get('/', function(req, res, next) {
   }
 });
 
+router.post('/:id/add', function(req, res, next) {
+  console.log(req.params);
+  var body = JSON.parse(Object.keys(req.body)[0]);
+  console.log(body);
+})
+
 module.exports = router;
