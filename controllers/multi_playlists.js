90c32f54a9c3c3d41ded8dbfaa4923611a4d7368
Kush Rastogi <krustey@gmail.com>
2018-08-03 19:11:32 -0700

    working towards edit name

--- controllers/multi_playlists.js
+++ controllers/multi_playlists.js
@@ -63,6 +63,25 @@ router.get('/:multi_id', function(req, res, next) {
   }
 });
 
+router.post('/:multi_id/editName', function(req, res, next) {
+  var body = JSON.parse(Object.keys(req.body)[0]);
+  console.log(body);
+
+  db.get().collection("spotify_sessions").update({
+    _id: new ObjectID(req.params.id)
+  }, {
+    $update: {
+      "name": body.name
+    }
+  },
+  function(err, count, status) {
+    console.log(err);
+    console.log(status);
+  });
+
+  res.send();
+})
+
 router.post('/:id/add', function(req, res, next) {
   console.log(req.params);
   var body = JSON.parse(Object.keys(req.body)[0]);
